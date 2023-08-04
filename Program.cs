e8fa3e59d7d03b8a6f398597a9ea4a7a03b306a0
Đàm Quốc Dân <124369155+damquocdan@users.noreply.github.com>
2023-08-04 12:24:56 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace BTLab04._2Cshap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NhanVienBanHang nvbh = new NhanVienBanHang();
+            Console.WriteLine("Nhan vien ban hang");
+            nvbh.HienThi("damquocdan", "tuson");
+            Console.WriteLine("Luong{0}",nvbh.TinhLuong(30, 1500)) ;
+            CongNhan cn = new CongNhan();
+            Console.WriteLine("cong nhan");
+            cn.HienThi("Manh QUan", "tienbao");
+            Console.WriteLine("Luong{0}", cn.TinhLuong(123, 454));
+
+            Console.ReadKey();
+        }
+    }
+}
